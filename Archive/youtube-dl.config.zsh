9b03b9539e9222ab23ebd42b7377010f95564885
Baffling Science <31553297+bafflingscience@users.noreply.github.com>
2021-03-25 10:00:53 -0400

    added Archive directory

---
+++ Archive/youtube-dl.config.zsh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Google API Keys
+ --api-authentication=AIzaSyBuCPBdNcaLMzBQ_7JicLmFqlqeqeLlYXA
+
+## Path to file of previously downloaded channel ids 
+ --download-archive=/home/user/Archive/downloaded.txt \
+
+## Path to file of channel urls that are downloaded from on each run 
+ --batch-file /home/user/Archive/channels.txt \
+
+## Output Template - Path and Format to save new downloads
+ -o /home/user/Videos/YouTube/%(uploader)s/[%(playlist_index)s].%(date)s%%(title)s.%(ext)s \
+ 
+## Dates to Download
+ --dateafter 20200307
+
